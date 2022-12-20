6ab5121390f012474a7aaa8d4718347976706b1b
NhanKhangJ <90268430+NhanKhangJ@users.noreply.github.com>
2022-12-19 17:19:11 -0700

    fix server

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const url = 'http://localhost:4000/contact'
+const url = ' https://long-garb-cod.cyclic.app/contact'
 
 export const createMessage = (newMessages) => axios.post(url, newMessages)
